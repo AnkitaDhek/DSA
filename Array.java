4c384250f25dc1de4a4c31a869254d659dc7a5bc
AnkitaDhek <122012285+AnkitaDhek@users.noreply.github.com>
2023-01-29 15:03:02 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,22 +1,23 @@
 class Array{
-
-    public static void reverseArray(int a[]){
-       int first = 0 ,last = a.length-1;
-       while(first<last){
-       int temp = a[last];
+//this code is to reverse an array
+    public static void reverseArray(int a[]){    //function
+       int first = 0 ,last = a.length-1;   //first is intialized with 0th index and  last is intialized  with last index 
+       while(first<last){   //as length of array is 6 so  the index of last no in array will a.length -1 that is it will be on 5 index
+     //here we are swapping the index
+         int temp = a[last];
          a[last] = a[first];
          a[first] =temp;
 
-         first++;
-         last--;
+         first++;//increment the index
+         last--;// decrementing the index
         }
        
     }
 public static void main(String[] args) {
 
     int a[] = {2,4,6,7,8,9};
-    reverseArray(a);
-  
+    reverseArray(a);//calling the function
+  //to print the array
     for(int i =0 ; i<a.length ; i++){
         System.out.print(a[i]+" ");
      }
